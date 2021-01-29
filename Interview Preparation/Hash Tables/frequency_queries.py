24395ceb9beec474efcd11513e3313f75da9fdef
Matheus cordeiro <matheus.cordeiro@ifpb.edu.br>
2021-01-28 23:26:07 -0300

    Insert a node at a specific position in a liked list

--- Interview Preparation/Hash Tables/frequency_queries.py
+++ Interview Preparation/Hash Tables/frequency_queries.py
@@ -26,6 +26,3 @@ def freqQuery(queries):
 			result.append(1) if occurrences.get(value) else result.append(0)
 
 	return result
-
-
-print(freqQuery([(1,89), (3,15), (1, 12), (1, 47), (1, 23), (1, 66), (2, 28), (3, 2), (2, 15), (1, 16), (3,16), (1,17), (1,73), (2,44), (3,14), (2,30), (2,38), (2,4), (1,4), (2,35), (1,28), (1,9), (1,68), (3,1), (3,33), (3,5)]))
